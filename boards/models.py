7e0b4840e09bde78a1d258a1590cb32a99e37575
Md Mahfuzur Rahman <shawon922@gmail.com>
2018-01-07 23:49:23 +0600

    Pagination with every topic based on number of posts

--- boards/models.py
+++ boards/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import Truncator
+import math
 
 
 class Board(models.Model):
@@ -25,6 +26,22 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.subject
+
+    def get_page_count(self):
+        count = self.posts.count()
+        pages = count / 20
+        return math.ceil(pages)
+
+    def has_many_pages(self, count=None):
+        if count is None:
+            count = self.get_page_count()
+        return count > 6
+
+    def get_page_range(self):
+        count = self.get_page_count()
+        if self.has_many_pages(count):
+            return range(1, 5)
+        return range(1, count+1)
 
     def get_replies(self):
         return self.posts.count() - 1
